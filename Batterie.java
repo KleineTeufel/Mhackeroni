4786e82aa2aa48b433f1ca7d6b4269e278d2785d
KleineTeufel <76700422+KleineTeufel@users.noreply.github.com>
2021-01-10 18:38:33 +0100

    Update Batterie.java

--- Batterie.java
+++ Batterie.java
@@ -49,6 +49,21 @@ public class Batterie {
         E=Hp*N;
         System.out.println("I Kw ottenuti sono ordunque "+E);
   }
+     public double metodo_5(double eff, int Co, int I_input, int I_consumo){
+        double Tcarica;
+        //controllo del rapporto capacità/Ah
+        if (Co/I_input<=4){
+            //formula tempo di carica
+        Tcarica=(Co*eff/I_input-I_consumo);
+        System.out.print(" la batteria si caricherà in "+Tcarica);
+        System.out.println(" ore");
+        }
+        else{
+            System.out.println(" Stai friggendo la batteria. Ben fatto! ");
+            return 0;
+        }
+         return Tcarica;
+         }
     public void metodo6(int Ah, int A){
         // buona la pasta
         /**
